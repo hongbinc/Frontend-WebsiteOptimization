5291f7c5284f12e97676f4c2731c061f910f51ab
hongbinc <hongbin1992@hotmail.com>
2016-03-22 14:29:22 -0400

    add grunt files

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        responsive_images: {
+            dev: {
+                options: {
+                    engine: 'im',
+                    sizes: [{
+                        name: 'small',
+                        width: '100%',
+                        suffix: '_small',
+                        quality: 30
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'img',
+                    dest: 'images/'
+                }]
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'js/dist/Analytics.min.js': ['js/src/Analytics.js'],
+                    'js/dist/fontload.min.js': ['js/src/fontload.js'],
+                    'js/dist/perfmatters.min.js': ['js/src/perfmatters.js'],
+                    'views/js/dist/main.min.js':['views/js/src/main.js']
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "responsive-image","uglify" task.
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Default task(s).
+    grunt.registerTask('default', ['responsive_images'], ['uglify']);
+
+};
